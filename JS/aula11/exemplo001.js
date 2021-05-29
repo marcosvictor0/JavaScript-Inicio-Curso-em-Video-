73f740406d28f512938a8a6fcb07b9c0f145af95
marcosvictor0 <marcos_victor159@hotmail.com>
2021-05-29 20:08:17 -0300

    entrando nos if,else e else if, adc exemplos 01 02 e 03 e anotações

---
+++ JS/aula11/exemplo001.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}/hora`)
+if ( vel > 60) {
+    console.log("Você ultrapassou a velocidade permitida, MULTADO!") //condição simples
+}
+console.log("Dirija sempre usando o cinto de segurança!")
