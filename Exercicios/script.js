aa8903041dc8759fb3c355bd6bc5ee1c501944e0
marcosvictor0 <marcos_victor159@hotmail.com>
2021-06-20 18:12:20 -0300

    feito um exercicio com js,html e css

--- Exercicios/script.js
+++ Exercicios/script.js
@@ -1,1 +1,18 @@
-alert("Olá")
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("img")
+    var data = new Date()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora <= 12) {
+        img.src = './imgs/manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = './imgs/tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = './imgs/noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
