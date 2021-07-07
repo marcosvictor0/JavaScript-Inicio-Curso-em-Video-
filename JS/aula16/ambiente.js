afb8e49d451fb5532e046fc49a7b213b634a5ac2
marcosvictor0 <marcos_victor159@hotmail.com>
2021-07-07 17:12:40 -0300

    att feitas no exercicio do array

--- JS/aula16/ambiente.js
+++ JS/aula16/ambiente.js
@@ -2,12 +2,27 @@ let num = [5, 4, 3, 2, 1, 6]
 num.sort()
 //num.push(8) // ele cria por si só uma chave e coloca no ultimo lugar do array
 //num.sort() // coloca os números em ordem crescente 
-console.log(num.length) // vai mostrar quantas posições tem nesse array
+//console.log(num.length) // vai mostrar quantas posições tem nesse array
 
-console.log(num)
+//console.log(num)
 
 //console.log(num[0]) //mostra somente o valor que está na chave 0 do array
 
-for (let pos = 0;pos < num.length;pos++){
+//for (let pos = 0;pos < num.length;pos++){
+//    console.log(num[pos])
+//}
+/*
+for ( let pos in num) {
     console.log(num[pos])
+}
+*/
+num.indexOf(5) // ele vai procurar no vetor, onde está o número 6
+console.log(num)
+console.log(num.indexOf(6))
+let pos = num.indexOf(6)
+
+if ( pos == -1 ){
+    console.log("O valor não foi encontrado.")
+} else {
+    console.log(`o valor está na posição ${pos}`)
 }
